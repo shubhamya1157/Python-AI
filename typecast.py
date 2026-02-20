8b2d405ab2ff8414e40e5e487213b29bfd1beb55
shubhamya1157 <shubhamya.1157@gmail.com>
2026-02-20 23:39:58 +0530

    Your commit message

---
+++ typecast.py
@@ -0,0 +1,36 @@
+#convert data type 
+a = 1157
+b = "17"
+c = 11.1
+d = True
+e = "shubham"
+
+print(a)
+print(type(a))
+print(str(a))
+print(float(a))
+print(bool(a))
+
+print(b)
+print(type(b))
+print(str(b))
+print(float(b))
+print(bool(b))
+
+print(c)
+print(type(c))
+print(str(c))
+print(float(c))
+print(bool(c))
+
+print(d)
+print(type(d))
+print(str(d))
+print(float(d))
+print(bool(d))
+
+print(e)
+print(type(e))
+#print(int(d)) 
+#print(float(e))
+print(bool(e))
