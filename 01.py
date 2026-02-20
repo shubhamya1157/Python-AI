e203274c4171274f563fdf665b479a9ce72574bf
shubhamya1157 <shubhamya.1157@gmail.com>
2026-02-20 23:24:24 +0530

    Initial commit

---
+++ 01.py
@@ -0,0 +1,20 @@
+# creating veriable
+
+name = "shubham";#string type
+age = 18;#float type 
+
+#_name = shubham -> wrong beacuse varible both are is varible and shubham varible ha no value
+
+full_name ="shubham yadav" #we can use underscore in front, middle ,last to declar varible
+name_1157 ="shubh"# we can use number at last and middle not front
+full_1157_name = "shubham yadav"
+#1157_name = "shubham" -> this is wrong
+
+correct_name = True #boolean declaration
+
+#For print- use print() function
+print(name) #print name varible value = shubham
+
+#For checking type of varible use type
+print(type(name)) #print str for string
+
