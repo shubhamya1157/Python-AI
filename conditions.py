d936d790ab38323c7f981dc0ef74ebb439bd7420
shubhamya1157 <shubhamya.1157@gmail.com>
2026-02-21 11:31:10 +0530

    Your commit message

---
+++ conditions.py
@@ -0,0 +1,9 @@
+#Appling if else elif for checkig number is postive,negtive,zero
+
+num = int(input("Enter a number:"))
+if num > 0:
+    print("Your given number is postive")
+elif num == 0:
+    print("Your number is neither postive nor negtive")
+else:
+    print("Your number is negtive")
