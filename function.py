2c71496ebb8a86022173a0a11d7bcd88aa39aca4
shubhamya1157 <shubhamya.1157@gmail.com>
2026-02-21 19:28:51 +0530

    all ok

---
+++ function.py
@@ -0,0 +1,17 @@
+'''
+def intro(name):
+    print("hello ",name,"! Hacker from hack404")
+
+name = input("Enter your name:")
+intro(name)
+
+'''
+
+'''
+def intro(name):
+    print(f"hello {name}! Hacker from hack404")
+
+name = input("Enter your name:")
+intro(name)
+
+'''
